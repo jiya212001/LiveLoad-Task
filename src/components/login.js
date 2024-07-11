1b17701c31e88cfec1772ed9140f2b5416c65b73
jiya212001 <jiya.agarwal@deorwine.com>
2024-07-11 16:41:09 +0530

    Fixed react query issue

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { TextField, IconButton, InputAdornment } from "@mui/material";
@@ -9,6 +9,9 @@ import * as yup from "yup";
 import Button from "@mui/material/Button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useMutation, QueryClient, QueryClientProvider } from "react-query";
+import { toast } from "react-toastify";
+
 const schema = yup.object().shape({
   email: yup
     .string()
@@ -17,111 +20,107 @@ const schema = yup.object().shape({
   password: yup.string().required("Password is required"),
 });
 
-const LoginForm = ({ onSubmitSuccess }) => {
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoginForm queryClient={queryClient} />
+    </QueryClientProvider>
+  );
+};
+
+const LoginForm = ({ queryClient }) => {
   const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
   } = useForm({
     resolver: yupResolver(schema),
   });
 
   const [showPassword, setShowPassword] = useState(false);
-  const [isPending, setIsPending] = useState(false);
 
   const handlePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
   const login = async (formData) => {
-    try {
-      setIsPending(true);
-      const response = await axios.post(
-        "https://liveload-api.vercel.app/api/v1/login",
-        formData
-      );
-      const data = response.data;
-      setIsPending(false);
-      if (typeof onSubmitSuccess === "function") {
-        onSubmitSuccess(data); // Call onSubmitSuccess if it's a function
-      }
-    } catch (error) {
-      setIsPending(false);
-      // Handle error
-      console.error("Login error:", error);
-    }
+    const response = await axios.post(
+      "https://liveload-api.vercel.app/api/v1/login",
+      formData
+    );
+    return response.data;
   };
 
+  const mutation = useMutation(login, {
+    onSuccess: (data) => {
+      router.push("/success");
+    },
+    onError: (error) => {
+      toast.error(error?.response?.data);
+    },
+    queryClient,
+  });
+
   const onSubmit = (formData) => {
-    const requestData = {
-      username: formData.email,
-      password: formData.password,
-    };
-    login(requestData);
+    mutation.mutate(formData);
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          {...register("email")}
-          label="Email"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          type="email"
-          required
-          error={!!errors.email}
-          helperText={errors.email?.message}
-          sx={{ mb: 2, "& .MuiInputBase-input": { height: "1rem" } }}
-        />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        {...register("email")}
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        type="email"
+        required
+        error={!!errors.email}
+        helperText={errors.email?.message}
+        sx={{ mb: 2, "& .MuiInputBase-input": { height: "1rem" } }}
+      />
 
-        <TextField
-          {...register("password")}
-          label="Password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          type={showPassword ? "text" : "password"}
-          required
-          error={!!errors.password}
-          helperText={errors.password?.message}
-          sx={{ mb: 2, "& .MuiInputBase-input": { height: "1rem" } }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handlePasswordVisibility}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
+      <TextField
+        {...register("password")}
+        label="Password"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        type={showPassword ? "text" : "password"}
+        required
+        error={!!errors.password}
+        helperText={errors.password?.message}
+        sx={{ mb: 2, "& .MuiInputBase-input": { height: "1rem" } }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handlePasswordVisibility}
+                edge="end"
+              >
+                {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
 
-        <Button
-          onClick={() => router.push("/success")}
-          type="submit"
-          variant="contained"
-          fullWidth
-          margin="normal"
-          sx={{ mb: -12 }}
-        >
-          LogIn
-        </Button>
-      </form>
-      {isPending && (
-        <div className="overlay">
-          <div className="loading"></div>
-        </div>
-      )}
-    </>
+      <Button
+        type="submit"
+        variant="contained"
+        fullWidth
+        margin="normal"
+        sx={{ mb: -12 }}
+        disabled={mutation.isLoading}
+      >
+        LogIn
+      </Button>
+    </form>
   );
 };
 
-export default LoginForm;
+export default App;
