95c26ee7c9c9dc979b340f584517f84bb59f8430
jiya212001 <jiya.agarwal@deorwine.com>
2024-07-12 19:02:33 +0530

    side Menu bar for individual users

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,25 +1,170 @@
+"use client";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
+import BoyIcon from "@mui/icons-material/Boy";
+import AirlineSeatReclineNormalIcon from "@mui/icons-material/AirlineSeatReclineNormal";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import StarIcon from "@mui/icons-material/Star";
+import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ContactPhoneOutlinedIcon from "@mui/icons-material/ContactPhoneOutlined";
+import LiveHelpOutlinedIcon from "@mui/icons-material/LiveHelpOutlined";
+import AutoStoriesOutlinedIcon from "@mui/icons-material/AutoStoriesOutlined";
+import SettingsSuggestOutlinedIcon from "@mui/icons-material/SettingsSuggestOutlined";
 
-export default function success() {
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+const menuItems = [
+  { text: "Dashboard", icon: <DashboardIcon /> },
+  { text: "Individual Users", icon: <PersonOutlineOutlinedIcon /> },
+  { text: "Business Users", icon: <GroupOutlinedIcon /> },
+  { text: "Vendors", icon: <BoyIcon /> },
+  { text: "Drivers", icon: <AirlineSeatReclineNormalIcon /> },
+  { text: "Orders", icon: <FormatListBulletedIcon /> },
+  { text: "Reviews", icon: <StarIcon /> },
+  { text: "Transactions", icon: <ReceiptLongOutlinedIcon /> },
+  { text: "Withdrawals", icon: <CreditCardOutlinedIcon /> },
+  { text: "Item Categories", icon: <EventNoteIcon /> },
+  { text: "Vehicle Categories", icon: <LocalShippingIcon /> },
+  { text: "Notifications", icon: <NotificationsNoneOutlinedIcon /> },
+  { text: "Contacts Us", icon: <ContactPhoneOutlinedIcon /> },
+  { text: "FAQ's", icon: <LiveHelpOutlinedIcon /> },
+  { text: "CMS Pages", icon: <AutoStoriesOutlinedIcon /> },
+  { text: "Settings", icon: <SettingsSuggestOutlinedIcon /> },
+];
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Typography
-      variant="h5"
-      component="div"
-      sx={{
-        fontSize: "clamp(2.345rem)",
-        lineHeight: 1.57,
-        fontFamily: "sans-serif",
-        fontWeight: 400,
-        color: "green",
-        cursor: "pointer",
-        position: "absolute",
-        top: "3vh",
-        left: "3vw",
-        maxWidth: "90vw",
-        textAlign: "center",
-      }}
-    >
-      Congrats you are logged In Successfully
-    </Typography>
+    <Box sx={{ display: "flex", backgroundColor: "white" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Liveload's | Welcome Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem key={item.text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+      </Main>
+    </Box>
   );
 }
